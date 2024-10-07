ac65311390b4d1c03f3b20f40763a5322a684ff3
Tiago Gomes <tidgomes@gmail.com>
2024-10-07 16:13:57 -0300

    create Person entity

---
+++ src/main/java/com/tidz/people/model/Person.java
@@ -0,0 +1,61 @@
+package com.tidz.people.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Integer age;
+    @Column(nullable = false)
+    private String profession;
+
+    public Person() {
+
+    }
+
+    public Person(String profession, Integer age, String name, Long id) {
+        this.profession = profession;
+        this.age = age;
+        this.name = name;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+}
