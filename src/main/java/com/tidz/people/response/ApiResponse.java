56f9edb9412a80c2699ad1df4610da2a74ebf7b4
Tiago Gomes <tidgomes@gmail.com>
2024-10-07 16:37:04 -0300

    create default response class

---
+++ src/main/java/com/tidz/people/response/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.tidz.people.response;
+
+public class ApiResponse {
+
+    private String message;
+    private Object body;
+
+    public ApiResponse(String message, Object body) {
+        this.message = message;
+        this.body = body;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+}
