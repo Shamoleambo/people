c8ef97570cac2c951c101a95d4abfc4001f77240
Tiago Gomes <tidgomes@gmail.com>
2024-10-08 11:56:54 -0300

    modify order of controller params

--- src/main/java/com/tidz/people/model/Person.java
+++ src/main/java/com/tidz/people/model/Person.java
@@ -21,11 +21,11 @@ public class Person {
 
     }
 
-    public Person(Long id, String profession, Integer age, String name) {
-        this.profession = profession;
-        this.age = age;
-        this.name = name;
+    public Person(Long id, String name, Integer age, String profession) {
         this.id = id;
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
     }
 
     public Long getId() {
