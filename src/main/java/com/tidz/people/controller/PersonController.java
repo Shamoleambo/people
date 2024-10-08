3017499205f3f73768d994f00156ca308acf5e3e
Tiago Gomes <tidgomes@gmail.com>
2024-10-08 12:22:53 -0300

    create end point to get all people

--- src/main/java/com/tidz/people/controller/PersonController.java
+++ src/main/java/com/tidz/people/controller/PersonController.java
@@ -5,10 +5,9 @@ import com.tidz.people.response.ApiResponse;
 import com.tidz.people.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/people")
@@ -27,5 +26,10 @@ public class PersonController {
         return ResponseEntity.ok(new ApiResponse("Success", savedPerson));
     }
 
+    @GetMapping("")
+    public ResponseEntity<ApiResponse> getAllPeople() {
+        List<Person> people = service.getAllPersons();
+        return ResponseEntity.ok(new ApiResponse("Success", people));
+    }
 
 }
